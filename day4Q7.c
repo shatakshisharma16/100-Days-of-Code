1df8f370f7ff19dc77e11ff066b6bb69a45e1924
shatakshi sharma <sharmasia853@gmail.com>
2025-09-11 05:23:04 +0530

    day 4

---
+++ day4Q7.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int a=4,b=5;
+    printf("before swapping a=%d b=%d\n",a,b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("after swapping the value a=%d and b=%d\n",a,b);
+    return 0;
+}
